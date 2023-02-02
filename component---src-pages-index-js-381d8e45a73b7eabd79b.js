22f9f536b3b8ca3e815d2157296a3d1963286ca0
Panos Lyrakis <panos.lyrakis@gmail.com>
2023-02-02 09:20:12 +0200

    Updates

---
+++ component---src-pages-index-js-381d8e45a73b7eabd79b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{6558:function(e,t,l){l.r(t),l.d(t,{Head:function(){return s}});var n=l(7294),a=l(8589),r=l(3100),u=l(8911),c=l(3049);t.default=()=>n.createElement(a.x,null,n.createElement(c.Z,null),n.createElement(r.xu,{p:8},n.createElement(u.x,{fontSize:"xl"},"Welcome to WPMUDEV Second Level Support")));const s=()=>n.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-381d8e45a73b7eabd79b.js.map
